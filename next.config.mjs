d4fc8f106a141a6cc407b147597e8c8b49bb5611
Himanshu48415 <himanshusingh48415@gmail.com>
2025-07-12 01:12:45 +0530

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         // protocol: "https",
+//         // hostname: "randomuser.me",
+//         domains: ["randomuser].me"],
+//       },
+//     ],
+//   },
+// };
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-      },
-    ],
+    domains: ['randomuser.me'],
   },
-};
-
+}
 export default nextConfig;
